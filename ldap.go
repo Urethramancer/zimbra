12439c5d13fd1647cbb439adaaf5bf9228f1eb38
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-07-21 03:12:05 +0200

    Account lookup for entire server and domains added.

--- ldap.go
+++ ldap.go
@@ -62,12 +62,33 @@ func (zc *ZimbraLDAP) Close() {
 	zc.conn.Close()
 }
 
-// GetUsers returns a list of all users.
-func (zc *ZimbraLDAP) GetUsers() {
+func (zc *ZimbraLDAP) getAccounts(scope string) ([]string, error) {
+	req := ldap.NewSearchRequest(scope, ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		"(&(objectClass=zimbraAccount))", []string{"dn", "cn"}, nil)
+	res, err := zc.conn.Search(req)
+	if err != nil {
+		return nil, err
+	}
 
+	list := []string{}
+	for _, e := range res.Entries {
+		list = append(list, UserToEmail(e.DN))
+	}
+
+	return list, nil
+}
+
+// GetUsers returns a list of all users.
+func (zc *ZimbraLDAP) GetUsers() ([]string, error) {
+	return zc.getAccounts("")
 }
 
 // GetUsers returns a list of all users in a domain.
-func (zc *ZimbraLDAP) GetUsersInDomain(domain string) {
+func (zc *ZimbraLDAP) GetUsersInDomain(domain string) ([]string, error) {
+	d, err := NewDomain(domain)
+	if err != nil {
+		return nil, err
+	}
 
+	return zc.getAccounts(d.BindDN)
 }
