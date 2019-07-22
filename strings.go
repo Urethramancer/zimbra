e7c976eb71f0555a16541b353daa300aef22aabd
Ronny Bangsund <github@neuralshortcircuit.com>
2019-07-22 19:24:06 +0200

    String/password-generator added.

---
+++ strings.go
@@ -0,0 +1,22 @@
+package zimbra
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+// GenString makes random strings of any length for passwords and such.
+func GenString(size int) string {
+	// Somewhat password-friendly.
+	valid := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#()*-.,")
+	pw := make([]byte, size)
+	for i := 0; i < size; i++ {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(valid))))
+		if err != nil {
+			return ""
+		}
+		c := valid[n.Int64()]
+		pw[i] = c
+	}
+	return string(pw)
+}
