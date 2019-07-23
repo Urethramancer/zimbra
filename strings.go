d6e46e61deea443f3590323c6a11cac824bac75d
Ronny Bangsund <github@neuralshortcircuit.com>
2019-07-23 03:03:07 +0200

    Safer password string, as Zimbra seems to have trouble with some symbols.

--- strings.go
+++ strings.go
@@ -8,7 +8,7 @@ import (
 // GenString makes random strings of any length for passwords and such.
 func GenString(size int) string {
 	// Somewhat password-friendly.
-	valid := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#()*-.,")
+	valid := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#-.")
 	pw := make([]byte, size)
 	for i := 0; i < size; i++ {
 		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(valid))))
